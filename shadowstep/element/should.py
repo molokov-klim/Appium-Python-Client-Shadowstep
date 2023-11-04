283b7cc3c36ad191e88640eb1e701dc8437467ac
k.molokov <k.molokov@atol.ru>
2023-11-05 02:43:10 +0300

    base structure

---
+++ shadowstep/element/should.py
@@ -0,0 +1,24 @@
+
+"""
+have:
+    attr
+    id
+    resource-id
+    index
+    package
+    class
+    text
+    bounds
+
+be:
+    checked
+    checkable
+    enabled
+    focusable
+    focused
+    long_clickable
+    password
+    scrollable
+    selected
+    displayed
+"""
