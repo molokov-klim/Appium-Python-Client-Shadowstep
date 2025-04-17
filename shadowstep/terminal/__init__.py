c640a0c401045c62415bb24f5dff39f39b335730
k.molokov <k.molokov@atol.ru>
2025-04-17 22:52:33 +0300

    0.8.4 rm DeviceInterface

--- shadowstep/terminal/__init__.py
+++ shadowstep/terminal/__init__.py
@@ -1,12 +1,8 @@
-from shadowstep.terminal.device_interface import DeviceInterface
-from shadowstep.terminal.device_factory import DeviceFactory
 from shadowstep.terminal.adb import Adb
 from shadowstep.terminal.terminal import Terminal
 from shadowstep.terminal.transport import Transport
 
 __all__ = [
-    'DeviceInterface',
-    'DeviceFactory',
     'Adb',
     'Terminal',
     'Transport',
