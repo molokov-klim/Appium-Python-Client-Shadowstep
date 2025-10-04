4ca06c007efad9f00b51447e877d51ecac1b6538
Molokov Klim <ultrakawaii9654449192@gmail.com>
2025-10-04 13:15:56 +0300

    0.34.167 self-hosted-runners

---
+++ tests/test_integro/_pages/page_saved_networks.py
@@ -0,0 +1,75 @@
+import logging
+from typing import Any
+from shadowstep.decorators.decorators import current_page
+from shadowstep.element.element import Element
+from shadowstep.page_base import PageBaseShadowstep
+
+
+class PageSavedNetworks(PageBaseShadowstep):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.logger = logging.getLogger(__name__)
+
+    def __repr__(self) -> str:
+        return f"{self.name} ({self.__class__.__name__})"
+
+    @property
+    def edges(self) -> dict[str, Any]:
+        return {}
+
+    @property
+    def name(self) -> str:
+        return "Saved networks"
+
+    @property
+    def title(self) -> Element:
+        return self.shadowstep.get_element({
+        'content-desc': 'Saved networks',
+        'resource-id': 'com.android.settings:id/collapsing_toolbar',
+        'class': 'android.widget.FrameLayout'
+    })
+
+    @property
+    def navigate_up(self) -> Element:
+        return self.recycler.scroll_to_element({
+            'content-desc': 'Navigate up',
+            'class': 'android.widget.ImageButton'
+        })
+
+    @property
+    def other_networks(self) -> Element:
+        return self.recycler.scroll_to_element({
+            'text': 'Other networks',
+            'resource-id': 'android:id/title',
+            'class': 'android.widget.TextView'
+        })
+
+    @property
+    def androidwifi(self) -> Element:
+        return self.recycler.scroll_to_element({
+            'text': 'AndroidWifi',
+            'resource-id': 'android:id/title',
+            'class': 'android.widget.TextView'
+        })
+
+    @property
+    def androidwifi_summary(self) -> Element:
+        return self.androidwifi.get_sibling({
+            'resource-id': 'android:id/summary'
+        })
+
+    @property
+    def recycler(self) -> Element:
+        return self.shadowstep.get_element({
+            'resource-id': 'com.android.settings:id/content_parent',
+            'class': 'android.widget.ScrollView'
+        })
+
+    @current_page()
+    def is_current_page(self) -> bool:
+        try:
+            return self.title.is_visible()
+        except Exception as error:
+            self.logger.error(error)
+            return False
